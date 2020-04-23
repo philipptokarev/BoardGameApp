0c670de87c711e5db486f8ac5bd20ee688723fe7
Phil <tokarev.ph@gmail.com>
2020-04-23 13:02:03 +0300

    Fix interactor test

--- spec/interactors/contact_user_spec.rb
+++ spec/interactors/contact_user_spec.rb
@@ -1,9 +1,11 @@
 require "rails_helper"
 
 RSpec.describe ContactUser do
+  let(:user) { create(:user) }
+
   it "Contact user successfully" do
-    interactor = described_class.call(params: { name: "phil", email: "phil@yandex.ru", text: Faker::Lorem.sentence(5) })
-    expect(interactor).to be_a_failure
+    interactor = described_class.call(params: { user_id: user.id, name: "phil", email: "phil@yandex.ru", text: Faker::Lorem.sentence(5) })
+    expect(interactor).to be_a_success
   end
 
   it "Contact user unsuccessfully" do
